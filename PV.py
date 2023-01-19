fef43ca2976c9bbbeaef37a2eb5f4ba66e5db6c3
Ashwin <winash12@gmail.com>
2023-01-19 15:33:20 +0530

    adding vectorized changes

--- PV.py
+++ PV.py
@@ -111,36 +111,27 @@ class potential_vorticity:
         dj = abs(np.radians((lat[0]-lat[1])) * rearth)
         # North Pole
         
-        for i in range(0,lonLen):
-            s0 = s[0,i]
-            s1 = s[1,i]
-            if (s0 -999.99 and s1 > -999.99):
-                # Make sure derivative is  dq/dy = [q(north) - q(south)]/dlat
-                dsdy[0,i] = (s[0,i] - s[1,i])/dj
-            else:
-                dsdy[0,i] = -999.99
-        for i in range(0,lonLen):
-            if (s[-1,i] > -999.99 and s[-2,i] > -999.99):
-                # Make sure derivative is  dq/dy = [q(north) - q(south)]/dlat
-                dsdy[-1,i] = (s[-2,i] - s[-1,i])/dj
-            else:
-                dsdy[-1,i] = -999.99
-        #has_left = s[:-2,:] > -999.99
-        #has_right = s[:-2,:] > -999.99
-        #dsdy1 = np.where(has_left & has_right,(s[:-2,:] - s[2:,:])/(2.*dj),dsdy1)
-        #Interior grid point
-        for j in range(1,latLen-1):
-            for i in range(0,lonLen):
-                if (s[j-1,i] > -999.99 and s[j+1,i] > -999.99):
-                    # Make sure derivative is  dq/dy = [q(north) - q(south)]/dlat
-                    dsdy[j,i] = (s[j+1,i] - s[j-1,i])/(2.0*dj)
-                    #print(dsdy[j,i])
-                elif (s[j-1,i] < -999.99 and s[j+1,i] > -999.99 and s[j,i] > -999.99):
-                    dsdy[j,i] = (s[j+1,i] - s[j,i])/dj
-                elif (s[j-1,i] > -999.99 and s[j+1,i] < -999.99 and s[j,i] > -999.99):
-                    dsdy[j,i] = (s[j,i] - s[j-1,i])/dj
-                else:
-                    dsdy[j,i] = -999.99
+        hasNValue = s[0,:] > -999.99
+        hasNLeft = s[1,:] > -999.99
+
+        dsdy[0,:] = -999.99
+        dsdy[0,:] = np.where(hasNValue & hasNLeft, (s[0,:]-s[1,:])/dj,dsdy[0,:])
+        #South Pole
+        hasSRValue = s[-1,:] > -999.99
+        hasSR2Value = s[-2,:] > -999.99
+
+        dsdy[-1,:] = -999.99
+        dsdy[-1,:] = np.where(hasSRValue & hasSR2Value,(s[-2,:]-s[-1,:])/dj,dsdy[-1,:])
+
+
+        #Regular coordinates
+        has_value = s[1:-1, :] > -999.99
+        has_right = s[2:,:] > -999.99
+        has_left = s[:-2,:] > -999.99
+        dsdy[1:-1,:] = -999.99
+        dsdy[1:-1,:] = np.where(has_left & has_value,(s[2,:] - s[1:-1,:]) / dj, dsdy[1:-1,:])
+        dsdy[1:-1,:] = np.where(has_right & has_value,(s[1:-1,:] - s[:-2,:]) / dj, dsdy[1:-1,:])
+        dsdy[1:-1,:] = np.where(has_left & has_right,(s[2:,:] - s[:-2,:])/(2.*dj),dsdy[1:-1,:])
 
         return dsdy
 
