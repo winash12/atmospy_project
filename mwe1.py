45e6e43621a0f6996682bb8bf9acca255d0d92e9
Ashwin <winash12@gmail.com>
2023-09-15 10:09:26 +0530

    added vectorization code

--- mwe1.py
+++ mwe1.py
@@ -28,21 +28,21 @@ div850 = mpcalc.divergence(u, v)
 
 mask = ((vort850.latitude <= 13.5) & (vort850.latitude >= 5.0) & (vort850.longitude <= 202.) & (vort850.longitude >= 191.))
 vortmask = vort850.where(mask)
+
 vortmask = vortmask.fillna(0.0)
 divmask = div850.where(mask)
 divmas = div850.fillna(0.0)
 dx, dy = mpcalc.lat_lon_grid_deltas(vortmask.longitude, vortmask.latitude)
-dx = np.abs(dx)
-dy = np.abs(dy)
+
 
 upsi = xr.zeros_like(vortmask)
 print(upsi.shape)
 
 
 vpsi = xr.zeros_like(vortmask)
+print(vpsi.shape)
 uchi = xr.zeros_like(divmask)
 vchi = xr.zeros_like(divmask)
-
 x_ll = list(vortmask.longitude.values).index(191.0)
 x_ur = list(vortmask.longitude.values).index(202.0)
 y_ll = list(vortmask.latitude.values).index(5.0)
@@ -53,38 +53,49 @@ x_ur_subset = list(vortmask.longitude.values).index(220.0)
 y_ll_subset = list(vortmask.latitude.values).index(0.0)
 y_ur_subset = list(vortmask.latitude.values).index(30.0)
 
+i = np.abs(x_ll_subset-x_ur_subset)
+j = np.abs(y_ll_subset-y_ur_subset)
+
+istart = np.linspace(x_ll_subset,x_ur_subset,num = i,endpoint=False,dtype=np.int32)
 
 
+jstart = np.linspace(y_ur_subset,y_ll_subset,num=j,endpoint=False,dtype=np.int32)
 
-# Iterate over each model grid point (larger area).
-# i and j refer to x and y in the Oertel and Schemm (2021) equations.
+x = np.abs(x_ll-x_ur)
+y = np.abs(y_ll-y_ur)
+
+
+xstart = np.linspace(x_ll,x_ur,num = x,endpoint=False,dtype=np.int32)
+ystart = np.linspace(y_ll,y_ur,num=y,endpoint=False,dtype=np.int32)
+
+xindex,yindex = np.meshgrid(xstart,ystart)
+
+iindex = np.zeros((x,y))
+jindex = np.zeros((x,y))
+
 for i in range(x_ll_subset, x_ur_subset):
+
     for j in range(y_ur_subset, y_ll_subset): 
 
-# Computing the contribution to each other grid point (masked area).
-# x1 and y1 refer to x' and y' in the Oertel and Schemm (2021) equations.
-        for x1 in range(x_ll, x_ur):
-            for y1 in range(y_ur, y_ll):
-                outer_point = [i,j]
-                inner_point = [x1,y1]
-                if inner_point != outer_point:
-                    
-                # Compute x-x', y-y', and r^2...
-                    xdiff = (i-x1)*dx[y1,x1].magnitude
-                    ydiff = (j-y1)*dy[y1,x1].magnitude
-                    rsq = (xdiff*xdiff) + (ydiff*ydiff)
-                
+        iindex = i
+        jindex = j
+        xdiff = (i(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)-xindex(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset))*dx(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)
+        ydiff = (j(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)-yindex(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset))*dy(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)
+        rsq = (xdiff*xdiff) + (ydiff*ydiff)
+        upsi[j,i] = np.where(rsq > 0, vortmask(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)*-1.0*(ydiff/rsq)*dx(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)*dy(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset), 0.0).sum()
+        vpsi[j,i] = np.where(rsq > 0, vortmask(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)*-1.0*(xdiff/rsq)*dx(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset)*dy(y_ur_subset:y_ll_subset,x_ll_subset:x_ur_subset), 0.0).sum()
+        
+                #   xdiff = (i-x1)*dx[y1,x1].magnitude
+                 #   ydiff = (j-y1)*dy[y1,x1].magnitude
+                  #  rsq = (xdiff*xdiff) + (ydiff*ydiff)
                 # Compute the non-divergent flow contribution.
-                    upsi[j,i] += vortmask[y1,x1].values * -1.0 * (ydiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
-                    vpsi[j,i] += vortmask[y1,x1].values * (xdiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
-                
-                # Compute the irrotational flow contribution.
-                    #uchi[j,i] += divmask[y1,x1].values * (xdiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
-                    #vchi[j,i] += divmask[y1,x1].values * (ydiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
+                    #upsi[j,i] += vortmask[y1,x1].values * -1.0 * (ydiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
+                    #vpsi[j,i] += vortmask[y1,x1].values * (xdiff / rsq) * dx[y1,x1].magnitude * dy[y1,x1].magnitude
+                    #print(upsi[j,i].values)
+
+
 
 
 
 upsi[:,:] = (1/(2*np.pi)) * upsi[:,:]
 vpsi[:,:] = (1/(2*np.pi)) * vpsi[:,:]
-#uchi[:,:] = (1/(2*np.pi)) * uchi[:,:]
-#vchi[:,:] = (1/(2*np.pi)) * vchi[:,:]
